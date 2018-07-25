789a7c674e119fffa1d9a0d0c5d997adc37c9580
Caroline Berg <carolineeberg@gmail.com>
2018-07-25 16:16:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,58 +15,12 @@ class TicTacToe
     [2, 4, 6],
   ]
 
-  def display_board
+  def display_board(@board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-
-  def input_to_index(user_input)
-    user_input.to_i - 1
-  end
-
-  def move(index, token = "X")
-    board[index] = token
-  end
-
-  def position_taken?(index)
-    !(board[index] == " " || board[position] == nil)
-  end
-
-  def valid_move?(index)
-    index.between?(0, 8) && board[index] == " "
-  end
-
-  def turn_count
-    counter = 0
-    board.each do |token|
-      if token == "X" || token == "O"
-        counter += 1
-      end
-    end
-    counter
-  end
-
-  def current_player
-    if turn_count % 2 == 0
-      return "X"
-    else
-      return "O"
-    end
-  end
-  
-  def turn
-    puts "Please enter a move: 1-9: "
-    position = gets.strip
-    index = position.to_i - 1
-      if valid_move?(index)
-        token = current_player
-        move(index, token)
-        display_board
-      else
-        turn
-      end
 
 end
